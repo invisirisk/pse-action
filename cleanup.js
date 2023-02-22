be1d2030482f31316d98dcec30c4c2c89d81c653
Umesh Sirsiwal <umesh@sirsiwal.in>
2023-02-22 12:15:14 -0500

    test

--- cleanup.js
+++ cleanup.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const http = require("@actions/http-client");
-const github = require("@actions/github");
+
 
 const fs = require('fs');
 
@@ -9,6 +9,17 @@ async function run() {
   try {
     core.info("cleanup");
     core.info(JSON.stringify(process.env));
+    client = new http.HttpClient("pse-action", [], { ignoreSslError: true });
+    let base = process.env.GITHUB_SERVER_URL + "/";
+    let repo = process.env.GITHUB_REPOSITORY;
+
+    let q = new URLSearchParams({
+      'builder': 'github',
+      'build_id': process.env.GITHUB_RUN_ID,
+      build_url: base + repo + "/actions/runs/" + process.env.GITHUB_RUN_ID + "/attempts/" + process.env.GITHUB_RUN_ATTEMPT,
+      status: process.env.GITHUB_RUN_RESULT
+    });
+    console.log(q.toString())
 
   } catch (error) {
     core.setFailed(error.message);
