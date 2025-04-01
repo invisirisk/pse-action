c33e027d9d44670d9bb21aac6da93eae7005c95e
allen.maharjan <allen.maharjan@tekvortex.com>
2025-04-01 17:45:39 +0545

    add proxy ip while setting ip tables

--- scripts/mode_intercept.sh
+++ scripts/mode_intercept.sh
@@ -354,7 +354,7 @@ setup_iptables() {
   # Add iptables rules
   run_with_privilege iptables -t nat -N pse
   run_with_privilege iptables -t nat -A OUTPUT -j pse
-  run_with_privilege iptables -t nat -A pse -p tcp --dport 443 -j REDIRECT --to-ports "$proxy_port"
+  run_with_privilege iptables -t nat -A pse -p tcp -d "$PROXY_IP" --dport 443 -j REDIRECT --to-ports "$proxy_port"
   #run_with_privilege iptables -t nat -A POSTROUTING -j MASQUERADE
   
   log "iptables rules set up successfully"
